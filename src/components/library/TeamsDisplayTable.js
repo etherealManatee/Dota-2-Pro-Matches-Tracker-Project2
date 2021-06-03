919891557c7671f4ae9ffc24752189f422334abf
hizashi <zytwp@hotmail.com>
2021-06-04 02:24:44 +0800

    final final commit

--- src/components/library/TeamsDisplayTable.js
+++ src/components/library/TeamsDisplayTable.js
@@ -13,7 +13,7 @@ function TeamsDisplayTable({teams}) {
     let filtered = teams.filter(filtering)
     useEffect(()=>{
         setFilteredTeams(filtered)
-    },[filtered])
+    },[teams])
 
     return (
         <Container>
